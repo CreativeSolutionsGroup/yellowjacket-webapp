3117852eab4cfbf92abd1a5060783e16596fa765
spbills <spencebills0@gmail.com>
2022-04-11 13:55:21 -0400

    Display studen id

--- src/components/Student.tsx
+++ src/components/Student.tsx
@@ -1,7 +1,10 @@
 import { StudentModel } from "../models/students";
 
-export const Student = ({ student }: {student: StudentModel}) => {
+export const Student = ({ student }: { student: StudentModel }) => {
   return (
-    <h1 className="font-bold text-3xl sm:text-5xl py-10">{student.last_name}, {student.first_name}</h1>
+    <div className="flex flex-col py-10">
+      <h1 className="font-bold text-3xl sm:text-5xl mb-3">{student.last_name}, {student.first_name}</h1>
+      <p className="text-xl">{student.student_id}</p>
+    </div>
   )
 }
