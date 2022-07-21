ae9aa48950ca4feb30f9fc4faebe52a9c43d68ed
SpBills <spencebills0@gmail.com>
2022-07-21 22:15:16 +0000

    Fixed strange dep ref issue

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,4 +1,4 @@
-import { getAuth } from "@firebase/auth";
+import { getAuth } from "firebase/auth";
 import { PropsWithChildren, useEffect, useState } from "react"
 import { Login } from "./Login"
 
