ac06a4012190d4519a459a04f93997ac0c102cb7
spbills <spencebills0@gmail.com>
2022-08-15 16:02:47 -0400

    Added confirm modal

--- src/views/SearchPage.tsx
+++ src/views/SearchPage.tsx
@@ -5,18 +5,24 @@ import { Student } from "../components/Student";
 import { StudentModel } from "../models/Students";
 import { checkInStudent, getAllCheckedIn, getAllSortedStudents } from "../services/students"
 
+import Modal from "react-modal";
+
 export const SearchPage = () => {
   const [students, setStudents] = useState<Array<StudentModel>>([]);
   const [filteredStudents, setFilteredStudents] = useState<Array<StudentModel>>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
+  const [selectedStudent, setSelectedStudent] = useState<StudentModel | null>(null);
+
+  const [checkInModalOpen, setCheckInModalOpen] = useState<boolean>(false);
+
   const getStudents = async () => {
     let stu = getAllSortedStudents();
     let checkedIn = getAllCheckedIn();
-    
+
     const val = await Promise.all([stu, checkedIn]);
     const isProd = import.meta.env.MODE === "production";
-    let parsedStudents = val[0].filter(s => 
+    let parsedStudents = val[0].filter(s =>
       (!!s.last_name) &&
       (val[1].findIndex(c => c.student_id === s.student_id) < 0) &&
       (isProd ? s.sting_group_id !== "STING_GROUP_TEST" : s.sting_group_id === "STING_GROUP_TEST")
@@ -26,7 +32,15 @@ export const SearchPage = () => {
     setLoading(false);
   }
 
+  const toggleOpenCheckIn = (stu: StudentModel | null) => {
+    setCheckInModalOpen(!checkInModalOpen);
+
+    setSelectedStudent(stu);
+  }
+
   const checkIn = async (stu: StudentModel) => {
+    setCheckInModalOpen(false);
+
     try {
       setLoading(true);
       // console.log(stu)
@@ -56,7 +70,7 @@ export const SearchPage = () => {
 
             {filteredStudents.map((s, i) => {
               return (
-                <button key={i} className="w-9/12 my-5 rounded-xl hover:text-white hover:bg-black cursor-pointer border border-black" type="button" onClick={() => checkIn(s)}>
+                <button key={i} className="w-9/12 my-5 rounded-xl hover:text-white hover:bg-black cursor-pointer border border-black" type="button" onClick={() => toggleOpenCheckIn(s)}>
                   <Student student={s} />
                 </button>
               )
@@ -64,6 +78,20 @@ export const SearchPage = () => {
           </div>
         </div>
       }
+
+      <Modal isOpen={checkInModalOpen}>
+        <div className="flex flex-col h-full justify-between">
+          <div>
+            <h2 className="text-3xl">Are you sure?</h2>
+            <p>This will send out up to 7 text messages.</p>
+          </div>
+
+          <div className="flex items-end flex-col w-full">
+            <button className="w-full h-24 my-5 rounded-xl bg-green-300 cursor-pointer border border-black" type="button" onClick={() => checkIn(selectedStudent!)}>Yes</button>
+            <button className="w-full h-24 my-5 rounded-xl bg-red-300 cursor-pointer border border-black" type="button" onClick={() => toggleOpenCheckIn(null)}>No</button>
+          </div>
+        </div>
+      </Modal>
 
     </div>
   )
